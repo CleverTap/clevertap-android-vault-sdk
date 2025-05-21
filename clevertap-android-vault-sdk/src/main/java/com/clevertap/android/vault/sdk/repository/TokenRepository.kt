31e5e44cf8a5878714c3d3ee6fb080cb185c2289
piyush-kukadiya <piyush.kukadiya@clevertap.com>
2025-05-21 13:57:54 +0530

    feat(vault_repo): add batch tokenization and de-tokenization operation to repository interface SDK-4798

--- clevertap-android-vault-sdk/src/main/java/com/clevertap/android/vault/sdk/repository/TokenRepository.kt
+++ clevertap-android-vault-sdk/src/main/java/com/clevertap/android/vault/sdk/repository/TokenRepository.kt
@@ -1,5 +1,7 @@
 package com.clevertap.android.vault.sdk.repository
 
+import com.clevertap.android.vault.sdk.model.BatchDetokenizeResult
+import com.clevertap.android.vault.sdk.model.BatchTokenizeResult
 import com.clevertap.android.vault.sdk.model.DetokenizeResult
 import com.clevertap.android.vault.sdk.model.TokenizeResult
 
@@ -23,4 +25,19 @@ interface TokenRepository {
      */
     suspend fun detokenize(token: String): DetokenizeResult
 
+    /**
+     * Tokenizes multiple sensitive values in a batch operation without encryption
+     *
+     * @param values The list of sensitive values to tokenize
+     * @return The batch tokenization result
+     */
+    suspend fun batchTokenize(values: List<String>): BatchTokenizeResult
+
+    /**
+     * Detokenizes multiple tokens in a batch operation
+     *
+     * @param tokens The list of tokens to detokenize
+     * @return The batch detokenization result
+     */
+    suspend fun batchDetokenize(tokens: List<String>): BatchDetokenizeResult
 }
